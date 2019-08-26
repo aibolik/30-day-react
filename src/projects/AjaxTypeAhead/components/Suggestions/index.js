b1f9d13eb1a1147035076c4e23f194577b964526
Aibol Kussain <aibolikdev@gmail.com>
2019-08-26 07:48:14 +0200

    fix(ahax-type-ahead): delete implementation of Suggestions for starter

--- src/projects/AjaxTypeAhead/components/Suggestions/index.js
+++ src/projects/AjaxTypeAhead/components/Suggestions/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 
-import Item from "../SuggestionItem";
-
 const Suggestions = ({ data }) => {
-  return (
-    <div>
-      {data.hits.map(post => (
-        <Item key={post.objectID} {...post} />
-      ))}
-    </div>
-  );
+  return null;
 };
 
 export default Suggestions;
